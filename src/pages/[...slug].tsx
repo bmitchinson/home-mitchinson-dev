3b28094194c17036bfe8499c9999758eac4217cc
Ben Mitchinson <mitchinson.dev@gmail.com>
2023-05-30 00:06:26 -0500

    Add support for dynamic redirect urls

---
+++ src/pages/[...slug].tsx
@@ -0,0 +1,40 @@
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import Head from "next/head";
+import { Config } from "../utils/configuration";
+import { Client } from "@notionhq/client";
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const notion = new Client({
+    auth: Config.notionAPIKey,
+  });
+
+  const slugsQueryResults = (await notion.databases.query({
+    database_id: Config.slugsDatabaseID,
+  })) as { results: any[] };
+
+  const slugs = {} as { [key: string]: string };
+  slugsQueryResults.results.forEach(
+    ({ properties }: { properties: { url: any; slug: any } }) => {
+      const { url, slug } = properties;
+      slugs[slug.title[0].plain_text] = url.url;
+    }
+  );
+
+  const url = context.req.url || "/404";
+  const redirectURL = slugs[url] || `${Config.blogURL}${url}`;
+  return {
+    props: {
+      url: redirectURL,
+    },
+  };
+}
+
+export default function Redirect({ url }: { url: string }) {
+  return (
+    <>
+      <Head>
+        {url && <meta http-equiv="Refresh" content={`0; url=${url}`} />}
+      </Head>
+    </>
+  );
+}
