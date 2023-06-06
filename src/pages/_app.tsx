f0b5c20f31dcea1e7626a079af5ad0076b699559
Ben Mitchinson <mitchinson.dev@gmail.com>
2023-06-05 21:50:59 -0500

    Initial Vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { Montserrat } from "next/font/google";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={montserrat.className}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   );
 }
